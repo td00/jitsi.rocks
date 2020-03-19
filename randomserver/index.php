17ca78423909eff2a894c558dc7bec9bfd9d5b4b
Thies Mueller <tmueller@wobcom.de>
2020-03-19 19:26:50 +0100

    added nue2 & fsn2

--- randomserver/index.php
+++ randomserver/index.php
@@ -1,5 +1,5 @@
 <?php
-$urls = array("fi-hel-1", "fi-hel-2", "de-nue-1", "de-fsn-1");
+$urls = array("fi-hel-1", "fi-hel-2", "de-nue-1", "de-nue-2", "de-fsn-1", "de-fsn-2");
 $url = array_rand($urls);
 $forwardedurl = $urls[$url];
 header("Location: https://$forwardedurl.jitsi.rocks");
