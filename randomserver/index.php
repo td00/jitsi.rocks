ede130175e84ef48aa4482d0c2b55f3dfb8d9b3a
Thies Mueller <tmueller@wobcom.de>
2020-03-19 17:37:01 +0100

    added fi-hel-2

--- randomserver/index.php
+++ randomserver/index.php
@@ -1,5 +1,5 @@
 <?php
-$urls = array("fi-hel-1", "de-nue-1", "de-fsn-1");
+$urls = array("fi-hel-1", "fi-hel-2", "de-nue-1", "de-fsn-1");
 $url = array_rand($urls);
 $forwardedurl = $urls[$url];
 header("Location: https://$forwardedurl.jitsi.rocks");
