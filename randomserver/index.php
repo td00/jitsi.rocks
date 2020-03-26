822270567fb91f03f49c4b91b3ee9d03a76c3859
Thies Mueller <tmueller@wobcom.de>
2020-03-26 21:22:10 +0100

    Added de-fsn-3

--- randomserver/index.php
+++ randomserver/index.php
@@ -1,5 +1,5 @@
 <?php
-$urls = array("de-wob-1", "fi-hel-1", "fi-hel-2", "de-nue-1", "de-nue-2", "de-fsn-1", "de-fsn-2", "us-nyc-1", "gb-lon-1", "nl-ams-1");
+$urls = array("de-wob-1", "fi-hel-1", "fi-hel-2", "de-nue-1", "de-nue-2", "de-fsn-1", "de-fsn-2", "de-fsn-3", "us-nyc-1", "gb-lon-1", "nl-ams-1");
 $url = array_rand($urls);
 $forwardedurl = $urls[$url];
 header("Location: https://$forwardedurl.jitsi.rocks");
